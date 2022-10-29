3c02de82f46e87d2972aa6392d4565f76f19dbb0
Christer Bjoerk <Chriz.gamedev@gmail.com>
2022-10-29 15:44:56 +0200

    Quitbutton

---
+++ Assets/Script/Interactions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interactions : MonoBehaviour
+{
+    public void QuitGameButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    } 
+}
