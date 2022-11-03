5ca1f581accf0beab669c6e08208fc8be929ab8a
Christer Bjoerk <Chriz.gamedev@gmail.com>
2022-11-03 09:53:18 +0100

    Unlocking the next level feature

--- Assets/Script/Gem.cs
+++ Assets/Script/Gem.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Gem : MonoBehaviour
@@ -19,7 +17,11 @@ public class Gem : MonoBehaviour
         {
             audioSource.PlayOneShot(gemCollecting);
             gemParticle.Play();
-        }
 
+            // Unlock the next level & save progress
+            GameManager.instance.UnlockNextLevel();
+
+            // Load the next level through SceneManager
+        }
     }
 }
--- Assets/Script/LevelSelector.cs
+++ Assets/Script/LevelSelector.cs
@@ -6,16 +6,13 @@ public class LevelSelector : MonoBehaviour
 {
     // IMPORTANT, the buttons needs to be sorted in numeric order for it to work
     [SerializeField] private List<Button> levelButtons = new List<Button>();
-    private int reachedLevel = 0;
 
     private void Start()
     {
-        // Only highlight levels the player has unlocked
-        reachedLevel = PlayerPrefs.GetInt("reachedLevel", 0);
-
+        // Only highlight levels the player has unlockek
         for (int i = 0; i < levelButtons.Count; i++)
         {
-            if (i > reachedLevel)
+            if (i > GameManager.instance.reachedLevel)
             {
                 // Else the levels are non interactable
                 levelButtons[i].interactable = false;
