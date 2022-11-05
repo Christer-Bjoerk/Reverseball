06a4bd5ff4e13ed5130b05ee2ac321441b8a1705
Christer Bjoerk <Christer_Bjoerk@protonmail.com>
2022-11-05 13:32:41 +0100

    Inverting joystick controls

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -19,11 +19,22 @@ public class GameManager : MonoBehaviour
 
         // Get all unlocked levels at the start of each gaming session
         reachedLevel = PlayerPrefs.GetInt("reachedLevel", reachedLevel);
+
+        Screen.autorotateToPortraitUpsideDown = true;
+
+        Screen.orientation = ScreenOrientation.PortraitUpsideDown;
     }
 
     public void UnlockNextLevel()
     {
         reachedLevel++;
         PlayerPrefs.SetInt("reachedLevel", reachedLevel);
+    }
+
+    public void DeleteSaveData()
+    {
+        PlayerPrefs.DeleteKey("reachedLevel");
+        // TODO
+        // Refresh Level Selection screen
     }
 }
